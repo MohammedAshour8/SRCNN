4ad09483bcbd3f38da5b9d99d8028763f9c8213f
mo-ashour <mohammedso.ashour@rai.usc.es>
2023-02-18 13:34:29 +0100

    Train and test v1

--- model.py
+++ model.py
@@ -5,9 +5,9 @@ class SRCNN(nn.Module):
         super(SRCNN, self).__init__()
         self.conv1 = nn.Conv2d(2, 64, kernel_size=9, padding=4)
         self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=1, padding=0)
         self.relu2 = nn.ReLU()
-        self.conv3 = nn.Conv2d(32, 2, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(64, 2, kernel_size=5, padding=2)
 
     def forward(self, x):
         out = self.conv1(x)
--- srcnn_train.py
+++ srcnn_train.py
@@ -14,15 +14,25 @@ def normalize(afai):
 # Load the data
 batch_size = 1
 train_dataset = ChlorophyllDataset('archivos_prueba/1km_750m/1km/', 'archivos_prueba/1km_750m/750m/')
+
+train_size = int(0.8 * len(train_dataset))
+test_size = len(train_dataset) - train_size
+train_dataset, test_dataset = th.utils.data.random_split(train_dataset, [train_size, test_size])
+
 train_loader = th.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = th.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+# train_loader = th.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
 # Create the model
 model = SRCNN()
 criterion = th.nn.MSELoss()
 optimizer = th.optim.Adam(model.parameters(), lr=0.001)
 
+model.train()
+
 # Train the model for 10 epochs
-epochs = 100
+epochs = 300
 for epoch in tqdm(range(epochs)):
     for (low_res, high_res) in train_loader:
         optimizer.zero_grad()
@@ -30,54 +40,15 @@ for epoch in tqdm(range(epochs)):
         loss = criterion(outputs, high_res)
         loss.backward()
         optimizer.step()
-    print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss.item():.4f}')
 
-# make a prediction with the model
-low_res_file = nc.Dataset('archivos_prueba/MODIS_TERRA_AFAI_MODIS_TERRA_AFAI.nc')
-high_res_file = nc.Dataset('archivos_prueba/VIIRS_NOAA20_AFAI_VIIRS_NOAA20_AFAI.nc')
+# test the model
+model.eval()
+test_loss = 0
+with th.no_grad():
+    for (low_res, high_res) in test_loader:
+        outputs = model(low_res)
+        test_loss += criterion(outputs, high_res)
+    print(f'Test Loss: {test_loss / len(test_loader):.4f}')
 
-low_res_lat = low_res_file['lat'][:]
-low_res_lon = low_res_file['lon'][:]
-high_res_lat = high_res_file['lat'][:]
-high_res_lon = high_res_file['lon'][:]
-low_res_lon, low_res_lat = np.meshgrid(low_res_lon, low_res_lat)
-high_res_lon, high_res_lat = np.meshgrid(high_res_lon, high_res_lat)
-
-low_res_data = low_res_file['afai'][:]
-high_res_data = high_res_file['afai'][:]
-low_res_data = np.nan_to_num(low_res_data, nan=0)
-high_res_data = np.nan_to_num(high_res_data, nan=0)
-
-low_res_data = th.from_numpy(low_res_data)
-high_res_data = th.from_numpy(high_res_data)
-
-_, low_res_lat, low_res_lon = low_res_data.shape
-
-low_res_data = F.resize(low_res_data, (high_res_data.shape[1], high_res_data.shape[2]), interpolation=F.InterpolationMode.BICUBIC)
-low_res_data = low_res_data.numpy()
-high_res_data = high_res_data.numpy()
-
-prediction = model(th.from_numpy(low_res_data).unsqueeze(0))
-prediction = prediction.detach().numpy()
-
-# Plot the results
-plt.figure(figsize=(10, 10))
-#plt.subplot(1, 3, 1)
-plt.title('Low resolution')
-plt.pcolormesh(high_res_lon, high_res_lat, low_res_data[0, :, :])
-plt.colorbar()
-plt.savefig('low_res.png')
-plt.clf()
-#plt.subplot(1, 3, 2)
-plt.figure(figsize=(10, 10))
-plt.title('High resolution')
-plt.pcolormesh(high_res_lon, high_res_lat, high_res_data[0, :, :])
-plt.colorbar()
-plt.savefig('high_res.png')
-plt.clf()
-#plt.subplot(1, 3, 3)
-plt.figure(figsize=(10, 10))
-plt.title('Prediction')
-plt.pcolormesh(high_res_lon, high_res_lat, prediction[0, 0, :, :])
-plt.colorbar()
-plt.savefig('prediction.png')
+# save the model
+th.save(model.state_dict(), 'SRCNN.pth')
